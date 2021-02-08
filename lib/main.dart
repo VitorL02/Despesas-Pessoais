0b9ecfa1757d284becb1afc8d5c5018841007379
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-02-08 19:39:47 -0300

    Mapeado transactions e transformado em um elemento visual no app

--- lib/main.dart
+++ lib/main.dart
@@ -46,11 +46,12 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text(
-              'Lista de transações',
-            ),
-            elevation: 5,
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
           )
         ],
       ),
