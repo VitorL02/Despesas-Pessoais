f2474ee07ae063074db0292390a5b3d298aeb509
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-02-08 20:06:17 -0300

    Valor,data,titulo,agora em uma Row,começando interface grafica das transações

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,17 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: _transactions.map((tr) {
               return Card(
-                child: Text(tr.title),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(tr.value.toString()),
+                    ),
+                    Column(children: <Widget>[
+                      Text(tr.title),
+                      Text(tr.date.toString()),
+                    ]),
+                  ],
+                ),
               );
             }).toList(),
           )
