1872d7668f180c9cc6c9d5b0f2d83cd0489312ad
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-02-11 19:31:47 -0300

    Adicionado os botões pra mostrar o formulario

--- lib/main.dart
+++ lib/main.dart
@@ -14,27 +14,37 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text(
-            'Despesas Pessoais',
-            style:
-                TextStyle(fontFamily: 'Raleway', fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: Colors.deepPurple[800]),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              child: Card(
-                child: Text('Grafico'),
-                elevation: 5,
-              ),
-            ),
-            TransactionUser(),
+        appBar: AppBar(
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {},
+            )
           ],
+          title: Text('Despesas Pessoais',
+              style: TextStyle(
+                  fontFamily: 'Raleway', fontWeight: FontWeight.bold)),
+          backgroundColor: Colors.deepPurple[800],
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                child: Card(
+                  child: Text('Grafico'),
+                  elevation: 5,
+                ),
+              ),
+              TransactionUser(),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.deepPurple[800],
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
   }
 }
