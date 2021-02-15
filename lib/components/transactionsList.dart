f82768b370b02511c4f20b1573ff4a43b8815e05
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-02-15 19:49:32 -0300

    Modificando a lista para um ListTile e estilizando

--- lib/components/transactionsList.dart
+++ lib/components/transactionsList.dart
@@ -40,38 +40,30 @@ class TransactionList extends StatelessWidget {
               itemBuilder: (ctx, index) {
                 final tr = transactions[index];
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.deepPurple[600],
-                          width: 2,
-                        )),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${tr.value.toStringAsFixed(2)}', //Interpolação de Strings,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.deepPurple[600],
-                          ),
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('R\$${tr.value}'),
                         ),
                       ),
-                      Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(tr.title,
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold)),
-                            Text(DateFormat('EEE d/MMM/y ').format(tr.date),
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                )),
-                          ]),
-                    ],
+                    ),
+                    title: Text(tr.title,
+                        style: TextStyle(
+                            fontFamily: 'Raleway',
+                            fontWeight: FontWeight.bold)),
+                    subtitle: Text(DateFormat('EEE d MMM y').format(tr.date)),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: null,
+                    ),
                   ),
                 );
               }),
