dce9737efec3ff508efdf3d17506314dc9197b86
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-02-16 20:11:25 -0300

    Responsividade no grafico,utilizando constrains

--- lib/components/chartBar.dart
+++ lib/components/chartBar.dart
@@ -13,41 +13,48 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Container(
-        height: 20,
-        child: FittedBox(
-          // Diminiue o texto pra 'caber no grafico',caso o valor seja muito alto
-          child: Text('R\$${value.toStringAsFixed(2)}'),
-        ),
-      ),
-      SizedBox(height: 5),
-      Container(
-        height: 60,
-        width: 10,
-        child: Stack(alignment: Alignment.bottomCenter, children: <Widget>[
+    return LayoutBuilder(
+      builder: (ctx, constrains) {
+        return Column(children: <Widget>[
           Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey,
-                width: 1.0,
+            height: constrains.maxHeight * 0.15,
+            child: FittedBox(
+              // Diminiue o texto pra 'caber no grafico',caso o valor seja muito alto
+              child: Text('\$${value.toStringAsFixed(2)}'),
+            ),
+          ),
+          SizedBox(height: constrains.maxHeight * 0.05),
+          Container(
+            height: constrains.maxHeight * 0.6,
+            width: 10,
+            child: Stack(alignment: Alignment.bottomCenter, children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
               ),
-              color: Color.fromRGBO(220, 220, 220, 1),
-              borderRadius: BorderRadius.circular(5),
-            ),
+              FractionallySizedBox(
+                heightFactor: percent,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurple[800],
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+              ),
+            ]),
           ),
-          FractionallySizedBox(
-            heightFactor: percent,
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.deepPurple[800],
-                  borderRadius: BorderRadius.circular(5)),
-            ),
-          ),
-        ]),
-      ),
-      SizedBox(height: 5),
-      Text(label),
-    ]);
+          SizedBox(height: constrains.maxHeight * 0.05),
+          Container(
+              height: constrains.maxHeight * 0.15,
+              child: FittedBox(child: Text(label))),
+        ]);
+      },
+    );
   }
 }
+//constrains.maxHeight Pega o valor do tamanho especifico que envolve o componente (resposividade)
